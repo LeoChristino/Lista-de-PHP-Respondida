3a86a54f8fdf0a248122ebc8446b67042e2b19d5
LeoChristino <Leo.chrissantos28@gmail.com>
2024-04-03 22:15:04 -0300

    Ex04

---
+++ ListadePHP-ex05.php
@@ -0,0 +1,17 @@
+<?php
+$h1 = 1.70;
+$h2 = 1.55;
+$h3 = 1.50;
+$idade1 = 18;
+$idade2 = 17;
+$idade3 = 12;
+$sexo1 = F;
+$sexo2 = M;
+$sexo3 = M;
+$ctrl = 0;
+while ($cont=0,$cont<=50,$cont++) {
+	if ($h1>$h2) {
+		
+	}
+}
+  ?> 
