b70143c5f1fec0be5fa871b7a5b4cd98c690dea7
Leonardo Christino <63653681+LeoChristino@users.noreply.github.com>
2024-04-10 21:51:20 -0300

    Add files via upload

--- ListadePHP-ex05.php
+++ ListadePHP-ex05.php
@@ -1,17 +1,57 @@
 <?php
 $h1 = 1.70;
-$h2 = 1.55;
-$h3 = 1.50;
 $idade1 = 18;
+$sexo1 = "F";
+
+$h2 = 1.98;
 $idade2 = 17;
-$idade3 = 12;
-$sexo1 = F;
-$sexo2 = M;
-$sexo3 = M;
+$sexo2 = "M";
+
+$h3 = 1.80;
+$idade3 = 22;
+$sexo3 = "M";
+
 $ctrl = 0;
-while ($cont=0,$cont<=50,$cont++) {
-	if ($h1>$h2) {
-		
+$cont = 0;
+$media = 0;
+while ($cont<5) {
+	if ($h1<$h2) {
+		$ctrl = $h1;
+		$h1 = $h2;
+		$h2 = $ctrl;
+	} else if ($h2<$h3) {
+		$ctrl = $h2;
+		$h2 = $h3;
+		$h3 = $ctrl;
 	}
+	$cont++;
 }
+$cont = 0;
+if ($sexo1=="F") {
+	$media = $media + $idade1;
+	$cont++;
+}
+if ($sexo2=="F") {
+	$media = $media + $idade2;
+	$cont++;
+}
+if ($sexo3=="F") {
+	$media = $media + $idade3;
+	$cont++;
+}
+$media = ($media/$cont);
+while ($cont<5) {
+	if ($idade1<$idade2) {
+		$ctrl = $h1;
+		$h1 = $h2;
+		$h2 = $ctrl;
+	} else if ($h2<$h3) {
+		$ctrl = $h2;
+		$h2 = $h3;
+		$h3 = $ctrl;
+	}
+	$cont++;
+}
+echo "A maior altura é ".$h1.", e a menor altura é ".$h3."</br>";
+echo "A média de idade das mulheres é: ".$media;
   ?> 
